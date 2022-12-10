f902076ed1c74e73a6dc370b0f10dd53753fd6ab
scott <scott.hagen@durham.ac.uk>
2022-12-10 14:52:25 +0000

    Included shell scripts for compiling to xspec and for modifying xspec.rc file for auto loading

--- compile_to_xspec.sh
+++ compile_to_xspec.sh
@@ -1,7 +1,5 @@
-echo "initpackage relagn lmod_relagn `pwd`/src/fortran_version/relagn_dir" | 
-xspec
-echo "initpacakge relqso lmod_relqso `pwd`/src/fortran_version/relqso_dir" | 
-xspec
+echo "initpackage relagn lmod_relagn.dat `pwd`/src/fortran_version/relagn_dir" | xspec
+echo "initpackage relqso lmod_relqso.dat `pwd`/src/fortran_version/relqso_dir" | xspec
 
 echo "lmod relagn `pwd`/src/fortran_version/relagn_dir" | xspec
 echo "lmod relqso `pwd`/src/fortran_version/relqso_dir" | xspec
