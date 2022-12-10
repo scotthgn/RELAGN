f67664bb6bfe0fbac9c83bedbf907ffceddd98a0
scott <scott.hagen@durham.ac.uk>
2022-12-10 14:48:19 +0000

    really fixed the shell script for compiling to xspec...

--- compile_to_xspec.sh
+++ compile_to_xspec.sh
@@ -1,7 +1,7 @@
-echo "initpackage relagn lmod_relagn `pwd`/src/fortran_version/relagn_dir | 
-xspec"
-echo "initpacakge relqso lmod_relqso `pwd`/src/fortran_version/relqso_dir | 
-xspec"
+echo "initpackage relagn lmod_relagn `pwd`/src/fortran_version/relagn_dir" | 
+xspec
+echo "initpacakge relqso lmod_relqso `pwd`/src/fortran_version/relqso_dir" | 
+xspec
 
-echo "lmod relagn `pwd`/src/fortran_version/relagn_dir | xspec"
-echo "lmod relqso `pwd`/src/fortran_version/relqso_dir | xspec"
+echo "lmod relagn `pwd`/src/fortran_version/relagn_dir" | xspec
+echo "lmod relqso `pwd`/src/fortran_version/relqso_dir" | xspec
